d8fb9ce78be04a49c6908b39543ce2425aac1e70
Sammylex01 <samueloyekoya12@gmail.com>
2021-11-27 00:08:47 +0100

    Reset Password
    Worked on reset password

---
+++ lib/src/views/screens/login_customer/components/background.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  final Size size;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/ellipse_1.png",
+                width: size.width * 0.35,
+              ),
+            ),
+            Positioned(
+                top: 0,
+                left: 1,
+                child:
+                Image.asset(
+                  "assets/images/ellipse_2.png",
+                  width: size.width * 0.35,
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
