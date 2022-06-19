9a6ec3e016b0cae7d30b4bd1e57936509290b32c
Okafor Onyekachukwu <okaforbestkid@gmail.com>
2022-06-19 16:14:54 +0100

    add: onboarding screen

--- lib/src/service/authentication_service.dart
+++ lib/src/service/authentication_service.dart
@@ -51,4 +51,59 @@ class AuthenticationService {
     print(customerOTPResponse.toString());
     return customerOTPResponse;
   }
+
+  // Service Provider Authentication
+
+  Future loginServiceProvider(String email, String password) async {
+    var loginSPResponse = await _helper.post(
+      "/login/",
+      body: {
+        "email": email,
+        "password": password,
+      },
+    );
+
+    print(loginSPResponse.toString());
+    return loginSPResponse;
+  }
+
+  Future signupServiceProvider(
+      {required String firstName,
+      required String lastName,
+      required String email,
+      required String phoneNumber,
+      required String password,
+      required String confirmPassword,
+      required String businessName}) async {
+    var createSPResponse = await _helper.post(
+      "/sp/register/",
+      body: {
+        'user': {
+          "first_name": firstName,
+          "last_name": lastName,
+          "email": email,
+          "phone_number": "phoneNumber",
+          "password": password,
+          "confirm_password": confirmPassword,
+          "location": "location",
+        },
+        'business_name': businessName,
+      },
+    );
+
+    print(createSPResponse.toString());
+    return createSPResponse;
+  }
+
+  Future verifyServiceProviderOTP({required String code}) async {
+    var spOTPResponse = await _helper.post(
+      "/otp/verification/",
+      body: {
+        "otp": code,
+      },
+    );
+
+    print(spOTPResponse.toString());
+    return spOTPResponse;
+  }
 }
